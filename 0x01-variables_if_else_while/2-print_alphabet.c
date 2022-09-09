71690bc559d7052c96bf08df4fb12db85e18640a
Adebola Soyemi <oluyemisisoyemi@gmail.com>
2022-09-09 13:28:05 -0700

    Modified the program

--- 0x01-variables_if_else_while/2-print_alphabet.c
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -5,14 +5,15 @@
  * Returns - 0 if success
  */
 
-int main (void)
+int main(void)
 {
 	char ch;
+
 	for (ch = 'a'; ch <= 'z'; ch++)
 	{
 		putchar(ch);
 	}
 	putchar('\n');
 
-	return(0);
+	return (0);
 }

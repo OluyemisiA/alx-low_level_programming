28b74bd372fbe20ba6407e19c5a2b6d202423c88
Adebola Soyemi <oluyemisisoyemi@gmail.com>
2022-09-19 02:27:48 -0700

    Added print_alphabet_x10

---
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -0,0 +1,25 @@
+#include "main.h"
+
+/**print_alphabet_x10 - prints the alphabet ten times
+ * Return: void
+ */
+
+void print_alphabet_x10(void)
+{
+	int cycle = 0;
+	char letter;
+
+	while (cycle < 10)
+	{
+		letter = 'a';
+		while (letter <= 'z')
+		{
+			_putchar(letter);
+			letter++;
+		}
+		_putchar('\n');
+
+		cycle++
+	}
+	
+}

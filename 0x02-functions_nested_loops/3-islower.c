c9955486b6300b8d15d7e1145f2267df0fa7b9fd
Adebola Soyemi <oluyemisisoyemi@gmail.com>
2022-09-19 02:38:49 -0700

    Added 3-islower.c

---
+++ 0x02-functions_nested_loops/3-islower.c
@@ -0,0 +1,18 @@
+#include "main.h"
+
+/**
+ * _islower - checks for lowercase c and returns 1 if the letter passed is lowercase c
+ * Return: 1 
+ */
+
+int _islower(int c)
+{
+	if (c >= 'a' && c <= 'z')
+	{
+		retunr (1);
+	}
+	else
+	{
+		return (0);
+	}
+}

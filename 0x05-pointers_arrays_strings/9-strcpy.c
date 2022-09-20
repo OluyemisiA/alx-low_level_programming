cd2b94137dcf1870fe822bbd56943a0a009f2442
Adebola Soyemi <oluyemisisoyemi@gmail.com>
2022-09-20 02:47:04 -0700

    Fixed 9-strcpy.c

--- 0x05-pointers_arrays_strings/9-strcpy.c
+++ 0x05-pointers_arrays_strings/9-strcpy.c
@@ -9,13 +9,14 @@
 
 char *_strcpy(char *dest, char *src)
 {
-	int i;
+	int i = 0;
 
-	for (i = 0; *(src + 1) != '\0'; i++)
+	while (*(src + 1))
 	{
-		dest[i] = *(src + i);
+		*(dest + i) = *(src + i);
+		i++
 	}
-	dest[i] = '\0';
+	*(dest + i) = '\0';
 
 	return (dest);
 }

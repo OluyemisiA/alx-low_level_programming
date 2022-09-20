05196a0aa3e0fdc1c3cd3fc9aabb4b3d75ded7c0
Adebola Soyemi <oluyemisisoyemi@gmail.com>
2022-09-19 22:49:19 -0700

    Initial Commit

---
+++ 0x05-ponters_arrays_strings/0-reset_to_98.c
@@ -0,0 +1,12 @@
+#include "main.h"
+
+/**
+ * Reset_to_98: Takes a pointer to the input then updates it to 98
+ * @n: input pointer
+ * Returns: void
+ */
+
+void reset_to_98(int *n)
+{
+	*n = 98;
+}

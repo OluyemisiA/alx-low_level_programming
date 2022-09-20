22873bf6a631bb8b80e6fd1a00437c2fc18a4f1b
Adebola Soyemi <oluyemisisoyemi@gmail.com>
2022-09-20 03:10:50 -0700

    Final commit

---
+++ 0x02-functions_nested_loops/7-print_last_digit.c
@@ -0,0 +1,25 @@
+#include "main.h"
+
+/**
+ * print_last - print the last digit of a number
+ * @n: input number
+ * Return: last digit of a number
+ */
+
+int print_last_digit(int n)
+{
+	int i;
+
+	if (n < 0)
+	{
+		i = -1 * (n % 10);
+		_putchar(i + '0');
+		return (i);
+	}
+	else
+	{
+		i = n % 10;
+		_putchar(i + '0');
+		return (i);
+	}
+}

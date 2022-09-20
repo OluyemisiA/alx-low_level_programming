7e02ecb4679d02028e853d17b04e4a9d3c8fe91c
Adebola Soyemi <oluyemisisoyemi@gmail.com>
2022-09-19 23:35:24 -0700

    Modified function

--- 0x05-pointers_arrays_strings/1-swap.c
+++ 0x05-pointers_arrays_strings/1-swap.c
@@ -1,14 +1,14 @@
 #include "main.h"
 
 /**
- * swap: swaps the values of two integers.
+ * swap_int: swaps the values of two integers.
  * @a: input integer 1
  * @b: input integer 2
  *
  * Return: void
  */
 
-void swap(int *a, int *b)
+void swap_int(int *a, int *b)
 {
 	int x = *a;
 	*a = *b;	

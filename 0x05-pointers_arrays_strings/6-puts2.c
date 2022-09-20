25985483e3f1ed994facbad21b8db51c903a2be9
Adebola Soyemi <oluyemisisoyemi@gmail.com>
2022-09-20 00:06:42 -0700

    Corrected function name

--- 0x05-pointers_arrays_strings/6-puts2.c
+++ 0x05-pointers_arrays_strings/6-puts2.c
@@ -1,12 +1,12 @@
 #include "main.h"
 
 /**
- * _puts- prints every other character of a string, starting with the first character
+ * puts2- prints every other character of a string, starting with the first character
  * @str: input string
  * Return: void
  */
 
-void _puts(char *str)
+void puts2(char *str)
 {
 	int i = 0;
 
